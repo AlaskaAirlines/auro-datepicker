0e465261e3ca0f66a70606b130f1dc08f3a264a8
Jason Baker <jason@capsule42.com>
2022-08-31 19:39:04 -0700

    docs(examples): make doc examples work 100% of the time on docsite #54

---
+++ demo/examples.js
@@ -0,0 +1,42 @@
+function initializeExample(element, callback, retryCount) {
+  const elem = document.querySelector(element);
+
+  if (!elem || !elem.ready) {
+    // If the component is not ready, retry until it is
+    if (!retryCount && retryCount != 0) {
+      retryCount = 0;
+    } else {
+      retryCount += 1;
+    }
+
+    if (retryCount < 10) {
+      setTimeout(initializeExample, 500, element, callback, retryCount);
+    } else {
+      console.error('Unable to initialize functional example code for:', element);
+    }
+  } else {
+    callback(elem);
+  }
+}
+
+/**
+ * Programmatically focus the datepicker input
+ */
+import { focusExample } from './../apiExamples/focus.js';
+
+(function(){
+  initializeExample('#focusExample', function(elem) {
+    focusExample(elem);
+  });
+}());
+
+/**
+ * Programmatically set value
+ */
+ import { valueExample } from './../apiExamples/value.js';
+
+ (function(){
+   initializeExample('#valueExample', function(elem) {
+    valueExample(elem);
+   });
+ }());
