0e63641e44f21deac3035cf79795b6f045131a81
Jason Baker <jason@capsule42.com>
2023-05-05 11:00:11 -0700

    docs(example_: remove commented out code

--- apiExamples/error.js
+++ apiExamples/error.js
@@ -2,13 +2,11 @@ const errorExample = document.querySelector('#errorExample');
 
 document.querySelector('#undefinedValueExampleBtnAddError').addEventListener('click', () => {
   console.warn('clickity click - add new error');
-  // errorExample.setAttribute('error', 'Custom error message New');
   errorExample.error = 'Custom New Error';
 })
 
 document.querySelector('#undefinedValueExampleBtnRemoveError').addEventListener('click', () => {
   console.warn('clickity click - remove error');
-  // errorExample.removeAttribute('error');
   errorExample.removeAttribute('error');
   console.warn('error example', errorExample.hasAttribute('error'));
 })
