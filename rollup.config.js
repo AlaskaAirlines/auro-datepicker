4e27d1ad9521a39b011d340f73823d0f55034636
Jason Baker <jason@capsule42.com>
2023-05-16 09:56:58 -0700

    ci(rollup): use modern rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,18 @@
-import { terser } from 'rollup-plugin-terser';
-import commonjs from '@rollup/plugin-commonjs';
-import minifyHTML from 'rollup-plugin-minify-html-literals';
-import resolve from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import serve from 'rollup-plugin-serve';
 
-const production = !process.env.ROLLUP_WATCH;
+const production = !process.env.ROLLUP_WATCH,
 
-const getSharedPlugins = (isLegacy) => [
-  resolve({
-    // in case of multiple lit-element versions (e.g. importing another auro component)
-    dedupe: ['lit-element', 'lit-html']
-  }),
-  commonjs(),
-  minifyHTML(),
-  terser()
-];
-
-const modernConfig = {
+ modernConfig = {
   input: {
-    ['auro-datepicker__bundled']: './src/auro-datepicker.js',
+    ['auro-datepicker__bundled']: './index.js',
   },
   output: {
     format: 'esm',
     dir: 'dist/'
   },
   plugins: [
-    ...getSharedPlugins(false),
+    nodeResolve(),
     !production &&
       serve({
         open: true,
