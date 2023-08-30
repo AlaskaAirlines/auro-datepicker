038a850783dc7616c0f2be73f82012ac99569272
Jason Baker <jason@capsule42.com>
2023-08-30 07:46:07 -0700

    docs(scripting): cleanup the doc build workflow

--- scripts/generateDocs.js
+++ scripts/generateDocs.js
@@ -112,17 +112,17 @@ function processReadme() {
 }
 
 /**
- * Compiles `./docTemplates/demo.md` -> `./demo/demo.md`
+ * Compiles `./docs/partials/index.md` -> `./demo/index.md`
  */
 
 function processDemo() {
   const callback = function(updatedContent, outputConfig) {
-    if (fs.existsSync('./demo/demo.md')) {
-      fs.readFile('./demo/demo.md', 'utf8', function(err, data) {
-        formatTemplateFileContents(data, './demo/demo.md');
+    if (fs.existsSync('./demo/index.md')) {
+      fs.readFile('./demo/index.md', 'utf8', function(err, data) {
+        formatTemplateFileContents(data, './demo/index.md');
       });
     } else {
-      console.log('ERROR: ./demo/demo.md file is missing');
+      console.log('ERROR: ./demo/index.md file is missing');
     }
   };
 
@@ -131,7 +131,7 @@ function processDemo() {
     outputDir: './demo'
   };
 
-  const markdownPath = path.join(__dirname, '../docs/partials/demo.md');
+  const markdownPath = path.join(__dirname, '../docs/partials/index.md');
 
   markdownMagic(markdownPath, configDemo, callback);
 }
