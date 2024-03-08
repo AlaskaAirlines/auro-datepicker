c76c4e788794652008ae7ccd1c90b3f0b61d368c
Jordan Jones <jmjones2463@gmail.com>
2024-03-08 01:10:52 -0800

    feat(focus): update input focus handling method #202

--- src/auro-datepicker.js
+++ src/auro-datepicker.js
@@ -219,10 +219,11 @@ export class AuroDatePicker extends LitElement {
 
   /**
    * Focuses the datepicker trigger input.
+   * @param {String} focusInput - Pass in 'returnInput' to focus on the return input. No parameter is needed to focus on the depart input.
    * @returns {void}
    */
-  focus() {
-    this.inputList[0].focus();
+  focus(focusInput) {
+    this.range && (focusInput === 'returnInput') ? this.inputList[1].focus() : this.inputList[0].focus();
   }
 
   /**
